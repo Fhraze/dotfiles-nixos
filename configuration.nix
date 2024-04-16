3cb15de95a9206f470876b60aae4d972011db376
Fhraze <bcfrazao@gmail.com>
2024-04-16 12:26:13 -0300

    I'll finish this later

--- configuration.nix
+++ configuration.nix
@@ -67,7 +67,7 @@
   # OpenDoas
   security.doas = {
     enable = true;
-    extraConfig = "permit persist setenv {PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin} :wheel\n"
+    extraConfig = "permit persist setenv {PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin} :wheel\npermit persist setenv { XAUTHORITY LANG LC_ALL } :wheel"
   };
 
   # Home Manager
